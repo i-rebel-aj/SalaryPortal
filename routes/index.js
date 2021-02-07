c613116f23b544dfb9fca570a9ac9d7a9c53686f
Namit Goel <namitgoel523@gmail.com>
2021-02-07 13:06:18 +0530

    login and 404 error page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 //Index Routes Goes Here
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res)=>{
+router.get("/home", (req, res)=>{
     res.render("home")
 })
-router.get("/login", (req, res)=>{
+router.get("/", (req, res)=>{
     res.render("login")
 })
 module.exports=router
