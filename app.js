4ef213c1e88c931ec1eca453d2d3afda1b961177
Namit Goel <namitgoel523@gmail.com>
2021-02-07 12:59:11 +0530

    login and 404 error page

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.set("view engine", 'ejs')
 
 app.use("/", indexRoutes);
 
+app.get('*', function(req, res){
+  res.render('404');
+});
+
 app.listen(3000, ()=>{
     console.log(`Server has started at post 3000`)
 })
