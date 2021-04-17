470620d6a5abff5408e32a3a655201a3b4111fe3
Akshay Jain <akshayjain1999.aj18@gmail.com>
2021-04-17 15:14:48 +0530

    Fixed UI

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,55 +1,55 @@
-const { Faculty, Staff, Management } = require("../models/User");
+const { Faculty, Staff, Management } = require("../models/User")
+//Admin Only access
 exports.addUser = async (req, res) => {
-  console.log('Request comming here')
-  const {type, email, pass, confirmpass, gender, instituteName, employeeId, department, enrolledDate, designation, retiredStaus, name}=req.body
-if(pass!==confirmpass){
-      req.flash('error', 'Passwords do not match')
-      res.redirect('/user/auth/signup')
-    //res.status(400).json({message: 'Passwords do not match'})
-  }else{
-      try{
-        const newUser={
-            email: email, 
-            password: pass, 
-            name: name,
-            gender: gender,
-            instituteName: instituteName,
-            department: department,
-            enrolledDate: enrolledDate,
-            enrollmentNumber: enrollmentNumber,
-            designation: designation
-        }
-        if(retiredStaus==='Yes'){
-            newUser.retiredStaus=true
-        }else if(retiredStaus==='No'){
-            newUser.retiredStaus=false
-        }
-        if(type==='Faculty'){
-            const faculty=new Faculty(newUser)
-            await faculty.save()
-        }else if(type==='Staff'){
-            const staff=new Staff(newUser)
-            await staff.save()
-        }else if(type==='Management'){
-            const management=new Management(newUser)
-            await management.save()
-        }else{
-            throw new Error('Invalid Type Selected')
-        } 
-        req.flash("success", "Email already exists")
-        res.redirect('/admin')
-      }catch(err){
-          //Handle Errors
-          console.log(err)
-          //Explain code 11000 here
+  const { type, email, pass, confirmpass, gender, employeeId, department, enrolledDate, designation, retiredStaus, name}=req.body
+    if(pass!==confirmpass){
+        req.flash('error', 'Passwords do not match')
+        res.redirect('/user/auth/signup')
+    }else{
+        try{
+            const newUser={
+                email: email, 
+                password: pass, 
+                name: name,
+                gender: gender,
+                instituteId:res.locals.currentUser.institute,
+                department: department,
+                enrolledDate: enrolledDate,
+                employeeId: employeeId,
+                designation: designation
+            }
+            if(retiredStaus==='Yes'){
+                newUser.retiredStaus=true
+            }else if(retiredStaus==='No'){
+                newUser.retiredStaus=false
+            }
+            if(type==='Faculty'){
+                const faculty=new Faculty(newUser)
+                await faculty.save()
+            }else if(type==='Staff'){
+                const staff=new Staff(newUser)
+                await staff.save()
+            }else if(type==='Management'){
+                const management=new Management(newUser)
+                await management.save()
+            }else{
+                throw new Error('Invalid Type Selected')
+            } 
+            req.flash("success", "Email already exists")
+            res.redirect('/admin')
+        }catch(err){
+            console.log(err)
             if (err.code === 11000) {
                 if (err.keyValue.email) {
                     req.flash("error", "Email already exists")
                     res.redirect('back')
                 }
             }
-            req.flash('error', 'Something went wrong')
+            req.flash('error', `Something went wrong ${err.message}`)
             res.redirect('back')
         } 
     }
+}
+exports.registerPosition =async(req, res)=>{
+    
 }
--- routes/admin.js
+++ routes/admin.js
@@ -7,12 +7,39 @@ const {isLoggedIn}=require('../middlewares/authentication')
 /*===============================
     All GET routes goes here
 =================================*/
-
+/*
+    @Route  GET  /admin
+    @Desc   To Admin to view his home page
+    @Access Private
+*/
 router.get('/', (req, res)=>{
     res.render('./admin/adminhome')
 })
+/*
+    @Route  GET  /admin/addemployee
+    @Desc   To Admin to add employee
+    @Access Private
+*/
 router.get('/addemployee',(req, res)=>{
     res.render('./admin/registerFaculty')
-} )
+})
+/*
+    @Route  GET  /admin/employee
+    @Desc   To View all employee
+    @Access Private
 
+*/
+router.get('/employee/leaves', (req, res)=>{
+    res.render('./admin/viewleaves')
+})
+
+/*
+    @Route  GET  /admin/employee/leaves
+    @Desc   To Admin to add employee
+    @Access Private
+
+*/
+router.get('/employee/leaves', (req, res)=>{
+    res.render('./admin/viewleaves')
+})
 module.exports=router
